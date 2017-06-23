839dcafcb29b6e286cd376ac030c666bb8bcec63
Andrew Pierce <apierce1992@gmail.com>
2017-06-23 16:49:46 -0400

    finally got axios to return requests

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 export default class App extends Component {
+
+  
+  componentDidMount() {
+    axios.get('http://swapi.co/api/people/').then((result) => {
+      console.log(result);
+    });
+  }
   render() {
     return (
       <div>React simple starter</div>
