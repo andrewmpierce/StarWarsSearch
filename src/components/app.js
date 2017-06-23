e27dc1bb925063d05ad7cbae414117c755889635
Andrew Pierce <apierce1992@gmail.com>
2017-06-23 17:29:03 -0400

    have search bar up with not working search feature

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import SearchBar from './search-bar';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-  
-  componentDidMount() {
-    axios.get('http://swapi.co/api/people/').then((result) => {
+    this.state = {
+      selectedCharacter: "Search for a character",
+     };
+
+     this.characterSearch('something');
+  }
+
+
+  characterSearch(type) {
+    axios.get(`http://swapi.co/api/people/`).then((result) => {
       console.log(result);
+      this.setState({selectedCharacter: result.data.results[0].name});
+
     });
   }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar onSearchTermChange={this.characterSearch} />
+        <div>{this.state.selectedCharacter}</div>
+      </div>
     );
   }
 }
